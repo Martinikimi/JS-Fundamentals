b8d318a12ab2a786947d8ccf32d55b3f350ee5f3
Martin Kimani <martinikimi7@gmail.com>
2025-11-06 21:37:45 +0300

    Create 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Create an array with the three messages
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Use a loop to print each line
+for (let i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}
