663b27701da83bd7a7772ae41724797811baeb53
Martin Kimani <martinikimi7@gmail.com>
2025-11-06 21:36:02 +0300

    Create 4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the two arguments (will be undefined if not provided)
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print in the format: "first is second"
+console.log(firstArg + " is " + secondArg);
